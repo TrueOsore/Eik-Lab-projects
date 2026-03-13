6bbe60f1a9fa1176a905f79e85ffeabe4ea863e2
TrueOsore <103216134+TrueOsore@users.noreply.github.com>
2026-03-13 14:03:41 +0100

    Add files via upload

---
+++ STM32 python communication.py
@@ -0,0 +1,21 @@
+#%%
+import serial
+
+# Config (must match ESP32 UART settings)
+ser = serial.Serial(
+    port = "COM5",
+    baudrate = 115200,
+    bytesize = serial.EIGHTBITS,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    timeout = 1
+)
+
+while True:
+    line = ser.readline().decode(errors="ignore").strip()
+    if line.startswith("Distance: "):
+        print(line)
+    
+    # if line.startswith("$GNGGA"):
+    #     print(line)
+# %%
